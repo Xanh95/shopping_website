a404ca80e14913052ccfbbfef682d75558fde6ac
xuan anh home <xuananhvolam2@gmail.com>
2023-04-11 18:36:48 +0700

    update info user

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,7 @@
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
- * 
+ *
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
@@ -11,70 +11,137 @@
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
-( function( window ) {
+(function (window) {
+  "use strict";
 
-    'use strict';
-    
-    // class helper functions from bonzo https://github.com/ded/bonzo
-    
-    function classReg( className ) {
-      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-    }
-    
-    // classList support for class management
-    // altho to be fair, the api sucks because it won't accept multiple classes at once
-    var hasClass, addClass, removeClass;
-    
-    if ( 'classList' in document.documentElement ) {
-      hasClass = function( elem, c ) {
-        return elem.classList.contains( c );
-      };
-      addClass = function( elem, c ) {
-        elem.classList.add( c );
-      };
-      removeClass = function( elem, c ) {
-        elem.classList.remove( c );
-      };
-    }
-    else {
-      hasClass = function( elem, c ) {
-        return classReg( c ).test( elem.className );
-      };
-      addClass = function( elem, c ) {
-        if ( !hasClass( elem, c ) ) {
-          elem.className = elem.className + ' ' + c;
-        }
-      };
-      removeClass = function( elem, c ) {
-        elem.className = elem.className.replace( classReg( c ), ' ' );
-      };
-    }
-    
-    function toggleClass( elem, c ) {
-      var fn = hasClass( elem, c ) ? removeClass : addClass;
-      fn( elem, c );
-    }
-    
-    var classie = {
-      // full names
-      hasClass: hasClass,
-      addClass: addClass,
-      removeClass: removeClass,
-      toggleClass: toggleClass,
-      // short names
-      has: hasClass,
-      add: addClass,
-      remove: removeClass,
-      toggle: toggleClass
+  // class helper functions from bonzo https://github.com/ded/bonzo
+
+  function classReg(className) {
+    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+  }
+
+  // classList support for class management
+  // altho to be fair, the api sucks because it won't accept multiple classes at once
+  var hasClass, addClass, removeClass;
+
+  if ("classList" in document.documentElement) {
+    hasClass = function (elem, c) {
+      return elem.classList.contains(c);
     };
-    
-    // transport
-    if ( typeof define === 'function' && define.amd ) {
-      // AMD
-      define( classie );
-    } else {
-      // browser global
-      window.classie = classie;
+    addClass = function (elem, c) {
+      elem.classList.add(c);
+    };
+    removeClass = function (elem, c) {
+      elem.classList.remove(c);
+    };
+  } else {
+    hasClass = function (elem, c) {
+      return classReg(c).test(elem.className);
+    };
+    addClass = function (elem, c) {
+      if (!hasClass(elem, c)) {
+        elem.className = elem.className + " " + c;
+      }
+    };
+    removeClass = function (elem, c) {
+      elem.className = elem.className.replace(classReg(c), " ");
+    };
+  }
+
+  function toggleClass(elem, c) {
+    var fn = hasClass(elem, c) ? removeClass : addClass;
+    fn(elem, c);
+  }
+
+  var classie = {
+    // full names
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass,
+    toggleClass: toggleClass,
+    // short names
+    has: hasClass,
+    add: addClass,
+    remove: removeClass,
+    toggle: toggleClass,
+  };
+
+  // transport
+  if (typeof define === "function" && define.amd) {
+    // AMD
+    define(classie);
+  } else {
+    // browser global
+    window.classie = classie;
+  }
+})(window);
+// js button
+$.fn.boom = function (e) {
+  var colors = [
+    // "#ffb3f6",
+    // "#7aa0ff",
+    // "#333",
+    "#14fd14",
+    "#45f3ff",
+    "#ffa549",
+  ];
+  var shapes = [
+    '<polygon class="star" points="21,0,28.053423027509677,11.29179606750063,40.97218684219823,14.510643118126104,32.412678195541844,24.70820393249937,33.34349029814194,37.989356881873896,21,33,8.656509701858067,37.989356881873896,9.587321804458158,24.70820393249937,1.0278131578017735,14.510643118126108,13.94657697249032,11.291796067500632"></polygon>',
+    '<path class="circle" d="m 20 1 a 1 1 0 0 0 0 25 a 1 1 0 0 0 0 -25"></path>',
+    '<polygon class="other-star" points="18,0,22.242640687119284,13.757359312880714,36,18,22.242640687119284,22.242640687119284,18.000000000000004,36,13.757359312880716,22.242640687119284,0,18.000000000000004,13.757359312880714,13.757359312880716"></polygon>',
+    '<polygon class="diamond" points="18,0,27.192388155425117,8.80761184457488,36,18,27.19238815542512,27.192388155425117,18.000000000000004,36,8.807611844574883,27.19238815542512,0,18.000000000000004,8.80761184457488,8.807611844574884"></polygon>',
+  ];
+
+  var btn = $(this);
+  var group = [];
+  var num = Math.floor(Math.random() * 50) + 30;
+
+  for (i = 0; i < num; i++) {
+    var randBG = Math.floor(Math.random() * colors.length);
+    var getShape = Math.floor(Math.random() * shapes.length);
+    var c = Math.floor(Math.random() * 10) + 5;
+    var scale = Math.floor(Math.random() * (8 - 4 + 1)) + 4;
+    var x = Math.floor(Math.random() * (150 + 100)) - 100;
+    var y = Math.floor(Math.random() * (150 + 100)) - 100;
+    var sec = Math.floor(Math.random() * 1700) + 1000;
+    var cir = $('<div class="cir"></div>');
+    var shape = $('<svg class="shape">' + shapes[getShape] + "</svg>");
+
+    shape.css({
+      top: e.pageY - btn.offset().top + 20,
+      left: e.pageX - btn.offset().left + 40,
+      transform: "scale(0." + scale + ")",
+      transition: sec + "ms",
+      fill: colors[randBG],
+    });
+
+    btn.siblings(".btn-particles").append(shape);
+
+    group.push({ shape: shape, x: x, y: y });
+  }
+
+  for (var a = 0; a < group.length; a++) {
+    var shape = group[a].shape;
+    var x = group[a].x,
+      y = group[a].y;
+    shape.css({
+      left: x + 50,
+      top: y + 15,
+      transform: "scale(0)",
+    });
+  }
+
+  setTimeout(function () {
+    for (var b = 0; b < group.length; b++) {
+      var shape = group[b].shape;
+      shape.remove();
     }
-    
-    })( window );
+    group = [];
+  }, 2000);
+};
+
+$(function () {
+  $(document).on("click", ".btn", function (e) {
+    $(this).boom(e);
+  });
+});
